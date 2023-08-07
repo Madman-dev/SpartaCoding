d8546cc08ce77330a1f25b6edeaad9900a9bf8c4
Jack Lee <dongjunlee222@gmail.com>
2023-08-07 17:08:31 +0900

    투두 데이터 관리 분리

--- TodoApp/TodoApp/TodoManager.swift
+++ TodoApp/TodoApp/TodoManager.swift
@@ -6,3 +6,23 @@
 //
 
 import Foundation
+
+class TodoManager{
+    static let shared = TodoManager()
+    private let userDefaults = UserDefaults.standard
+    private let todoKey = "Todos"
+    
+    func saveTodo(_ items: [Todo]) {
+        if let encodedData = try? JSONEncoder().encode(items) {
+            userDefaults.set(encodedData, forKey: todoKey)
+        }
+    }
+    
+    func loadTodo() -> [Todo] {
+        if let encodedData = userDefaults.data(forKey: todoKey),
+           let decodedItems = try? JSONDecoder().decode([Todo].self, from: encodedData) {
+            return decodedItems
+        }
+        return []
+    }
+}
